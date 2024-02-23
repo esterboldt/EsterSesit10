fadacbb63013bfc6a17d671749be700a3d2f5be3
esterboldt <esterboldt10@gmail.com>
2024-02-23 08:53:19 -0300

    pilude

---
+++ restaurante/pedido/script.js
@@ -0,0 +1,23 @@
+function comecar(){
+    var name = document.getElementById("exampleForControlInput1").value;
+
+    show(name);
+}
+
+function show(name){
+    document.getElementById("nomePrint").textContent = none;
+    var div = document.getElementById("selects-div");
+    if(nome && nome.trim() !== ""){
+        div.style.display = "block"
+        document.getElementById("exampleFormControlInput1").style.background = "none";
+    }else{
+        document.getElementById("error").style.display = "flex";
+
+    }
+
+}
+
+function fechar(){
+    document.getElementById("error").style.display = "none";
+    document.getElementById("exampleFormControlInput1").style.background = "black";
+}
